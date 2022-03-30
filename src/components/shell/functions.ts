ed3943fa0b4f94a2638ae571b52239645202e0cc
Siarhei Khonski <armonius@mail.ru>
2022-03-30 12:13:14 +0300

    feat: add the result processing

--- src/components/shell/functions.ts
+++ src/components/shell/functions.ts
@@ -7,12 +7,14 @@ export function calculateResult(expression: string[]): string {
   const [num1, , num2 = "0", isPercent = ""] = expression;
   const action = expression.length === 2 ? "+" : expression[1];
   let result: string = "0";
-  console.log("PERCENT>>>", !!isPercent)
   switch (action) {
-    case "+": result = (!!isPercent ? String(+num1 + +num1 / 100 * +num2) : String(+num1 + +num2)).slice(0, 10); break;
-    case "-": result = (!!isPercent ? String(+num1 - +num1 / 100 * +num2) : String(+num1 - +num2)).slice(0, 10); break;
-    case "X": result = (!!isPercent ? String(+num1 * +num2/100) : String(+num1 * +num2)).slice(0, 10); break;
-    case "/": result = (!!isPercent ? String(+num1 / (+num2 / 100)) : String(+num1 / +num2)).slice(0, 10);; break;
+    case "+": result = !!isPercent ? String(+num1 + +num1 / 100 * +num2) : String(+num1 + +num2); break;
+    case "-": result = !!isPercent ? String(+num1 - +num1 / 100 * +num2) : String(+num1 - +num2); break;
+    case "X": result = !!isPercent ? String(+num1 * +num2/100) : String(+num1 * +num2); break;
+    case "/": result = +num2 === 0 ? "Err" : !!isPercent ? String(+num1 / (+num2 / 100)) : String(+num1 / +num2); break;
   };
+  result = result.length > 10 && result.includes(".") ? result.slice(0, 10) : result;
+  result = result[result.length - 1] === "." ? result.slice(0, result.length -1) : result;
+  result = +result > 9999999999 || +result < -9999999999 ? ("^" + result).slice(0, 10) : result;
   return result;
 };
